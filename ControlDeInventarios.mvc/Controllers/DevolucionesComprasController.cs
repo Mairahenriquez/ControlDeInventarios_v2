b2dec711a20e47858f100e5f76f6e6292b0a186c
Maira <ch17013@ues.edu.sv>
2024-09-20 13:44:28 -0600

    DSI-85 Se agrega Controlador de devoluciones de compras

---
+++ ControlDeInventarios.mvc/Controllers/DevolucionesComprasController.cs
@@ -0,0 +1,19 @@
+﻿using ControlDeInventarios.mvc.Middlewares;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlDeInventarios.mvc.Controllers
+{
+    [AuthAttribute]
+    public class DevolucionesComprasController : Controller
+    {
+        // GET: DevolucionesCompras
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
