39d39c9914c43846b9d8570af09e4bbcc58828e8
Natanael <cs21062@ues.edu.sv>
2024-10-02 23:03:14 -0600

    Se agrega controlador de Cuentas Bancarias

---
+++ ControlDeInventarios.mvc/Controllers/TesoreriaBancosController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ControlDeInventarios.mvc.Controllers
+{
+    public class TesoreriaBancosController : Controller
+    {
+        // GET: TesoreriaBancos
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
