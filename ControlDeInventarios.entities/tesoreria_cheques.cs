d026a6a8f792f61404f9e08688bbe8eaad6c6dcd
Cristian Antonio Escalante Hernandez <eh18010@ues.edu.sv>
2024-10-07 22:07:24 -0600

    feat: crud de cheques

---
+++ ControlDeInventarios.entities/tesoreria_cheques.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlDeInventarios.entities
+{
+    public class tesoreria_cheques
+    {
+        [Key]
+        public int PK_codigo { get; set; }
+        public decimal monto { get; set; }
+        public DateTime fecha { get; set; }
+        public int FK_proveedores_compras { get; set; }
+        public int FK_tesoreria_bancos { get; set; }
+    }
+}
---
+++ ControlDeInventarios.entities/vw_tesoreria_cheques.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlDeInventarios.entities
+{
+    public class vw_tesoreria_cheques
+    {
+        [Key]
+        public int PK_codigo { get; set; }
+        public decimal monto { get; set; }
+        public DateTime fecha { get; set; }
+        public string documento { get; set; }
+        public string nombre { get; set; }
+    }
+}
